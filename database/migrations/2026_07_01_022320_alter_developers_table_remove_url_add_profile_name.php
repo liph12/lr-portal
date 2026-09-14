926189c1357ae7c2dbf99299bddf8c31e8387ae7
Wizzax <marklawrince730@gmail.com>
2026-09-14 13:57:30 +0800

    migrate

--- database/migrations/2026_07_01_022320_alter_developers_table_remove_url_add_profile_name.php
+++ database/migrations/2026_07_01_022320_alter_developers_table_remove_url_add_profile_name.php
@@ -1,30 +1,30 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+// use Illuminate\Database\Migrations\Migration;
+// use Illuminate\Database\Schema\Blueprint;
+// use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::table('developers', function (Blueprint $table) {
-            $table->string('profile_name')->nullable()->after('address');
-            $table->dropColumn('url');
-        });
-    }
+// return new class extends Migration
+// {
+//     /**
+//      * Run the migrations.
+//      */
+//     public function up(): void
+//     {
+//         Schema::table('developers', function (Blueprint $table) {
+//             $table->string('profile_name')->nullable()->after('address');
+//             $table->dropColumn('url');
+//         });
+//     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::table('developers', function (Blueprint $table) {
-            $table->dropColumn('profile_name');
-            $table->string('url')->nullable()->after('address');
-        });
-    }
-};
+//     /**
+//      * Reverse the migrations.
+//      */
+//     public function down(): void
+//     {
+//         Schema::table('developers', function (Blueprint $table) {
+//             $table->dropColumn('profile_name');
+//             $table->string('url')->nullable()->after('address');
+//         });
+//     }
+// };
